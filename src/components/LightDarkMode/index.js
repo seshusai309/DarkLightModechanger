618e4ffc39d595390b73cd94aa20dc1878bf5f93
seshu <saiseshar@gmail.com>
2024-08-20 02:47:18 +0000

    Updated button text

--- src/components/LightDarkMode/index.js
+++ src/components/LightDarkMode/index.js
@@ -3,26 +3,34 @@ import {Component} from 'react'
 import './index.css'
 
 class LightDarkMode extends Component {
-  state = {mode: true, value: 'card-container lightMode'}
+  state = {mode: true, value: 'card-container lightMode', btnText: 'Dark Mode'}
 
   changeMode = () => {
     this.setState(st => {
       if (st.mode) {
-        return {mode: false, value: 'card-container darkMode'}
+        return {
+          mode: false,
+          value: 'card-container darkMode',
+          btnText: 'Light Mode',
+        }
       }
-      return {mode: true, value: 'card-container lightMode'}
+      return {
+        mode: true,
+        value: 'card-container lightMode',
+        btnText: 'Dark Mode',
+      }
     })
   }
 
   render() {
-    const {value} = this.state
+    const {value, btnText} = this.state
 
     return (
       <div className="container">
         <div className={value}>
           <h1 className="heading">Click To Change Mode</h1>
           <button onClick={this.changeMode} className="btn" type="button">
-            Dark Mode
+            {btnText}
           </button>
         </div>
       </div>
